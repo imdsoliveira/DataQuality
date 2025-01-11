dac8a1ff577521ac356155d13ae08f992dfe181b
Douglas Oliveira <63203428+imdsoliveira@users.noreply.github.com>
2025-01-11 07:29:51 -0300

    add test

--- app/compoments/sidebar.py
+++ app/compoments/sidebar.py
@@ -1,29 +1,32 @@
 # components/sidebar.py
 import sys
 from pathlib import Path
+
 current_dir = Path(__file__).resolve().parent
 parent_dir = current_dir.parent
 sys.path.insert(0, str(parent_dir))
 
 import streamlit as st
-from functions.functions import pages_map
 from config.config import is_production
+from functions.functions import pages_map
 
 PAGES = pages_map()
+
 
 def create_sidebar():
     # Authenticate only if in production
     if is_production():
-        if not st.session_state.get('authentication_status'):
+        if not st.session_state.get("authentication_status"):
             st.switch_page("/login.py")
             return
     else:
         # If not in production, initialize the necessary session variables
-        if 'authentication_status' not in st.session_state:
-            st.session_state['authentication_status'] = True
-        if 'name' not in st.session_state:
-            st.session_state['name'] = 'Dev User'
-    st.markdown("""
+        if "authentication_status" not in st.session_state:
+            st.session_state["authentication_status"] = True
+        if "name" not in st.session_state:
+            st.session_state["name"] = "Dev User"
+    st.markdown(
+        """
     <style>
         /* Esconder elementos padrão do Streamlit */
         [data-testid="stSidebarNav"] {display: none !important;}
@@ -61,9 +64,11 @@ def create_sidebar():
             border-radius: 4px;
         }
     </style>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
     # Authenticate
-    if not st.session_state.get('authentication_status'):
+    if not st.session_state.get("authentication_status"):
         st.switch_page("/login.py")
         return
     with st.sidebar:
@@ -71,33 +76,33 @@ def create_sidebar():
         try:
             st.logo("./app/static/img/logo-azul.png", size="large")
         except:
-            #pass
+            # pass
             st.image("./app/static/img/logo-azul.png", width=50)
         # Start current_page
-        if 'current_page' not in st.session_state:
-            st.session_state['current_page'] = 'Home'
+        if "current_page" not in st.session_state:
+            st.session_state["current_page"] = "Home"
         # Navigation
         st.subheader("Páginas disponíveis")
         pagina = st.selectbox(
             "Selecione a página",
             list(PAGES.keys()),
-            index=list(PAGES.keys()).index(st.session_state['current_page']),
-            key='navigation_selectbox',
-            label_visibility="collapsed"
+            index=list(PAGES.keys()).index(st.session_state["current_page"]),
+            key="navigation_selectbox",
+            label_visibility="collapsed",
         )
-        
+
         st.divider()
 
         st.subheader(f'Welcome *{st.session_state["name"]}*')
 
-        if st.button('Sair'):
-            for key in ['authentication_status', 'username', 'name', 'current_page']:
+        if st.button("Sair"):
+            for key in ["authentication_status", "username", "name", "current_page"]:
                 if key in st.session_state:
                     del st.session_state[key]
-            st.switch_page('/login.py')
+            st.switch_page("/login.py")
             return
-        if pagina != st.session_state['current_page']:
-            st.session_state['current_page'] = pagina
+        if pagina != st.session_state["current_page"]:
+            st.session_state["current_page"] = pagina
             try:
                 arquivo = PAGES[pagina]
                 st.switch_page(f"pages/{arquivo}.py")
--- app/pages/home.py
+++ app/pages/home.py
@@ -1,17 +1,14 @@
 # pages/home.py
 import sys
 from pathlib import Path
+
 current_dir = Path(__file__).resolve().parent
 parent_dir = current_dir.parent
 sys.path.insert(0, str(parent_dir))
 
-from components.sidebar import create_sidebar
-
 import streamlit as st
-from functions.functions import (
-    config_pages, 
-    logged_in
-)
+from components.sidebar import create_sidebar
+from functions.functions import config_pages, logged_in
 
 config_pages()
 logged_in()
